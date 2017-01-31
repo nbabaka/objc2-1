8aa9377ae0706cd3ebe850a98a19709896e528bd
Nikolay Karataev <nikolay@karataev.net>
2017-01-31 11:04:29 +0300

    All files added :)

---
+++ Pods/Target Support Files/Pods-obj-c_2_1/Pods-obj-c_2_1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_obj_c_2_1 : NSObject
+@end
+@implementation PodsDummy_Pods_obj_c_2_1
+@end
---
+++ Pods/Target Support Files/Pods-obj-c_2_1/Pods-obj-c_2_1-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_obj_c_2_1VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_obj_c_2_1VersionString[];
+
